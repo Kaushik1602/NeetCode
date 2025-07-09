d7e1135c2d3f80d3676c8a4d54a121496aa27cda
Kaushik <bankarkaushik@gmail.com>
2025-07-10 01:59:08 +0530

    Minimum Window Substring with single hashmap

--- slidingWindow/MinimumWindowSubstring.java
+++ slidingWindow/MinimumWindowSubstring.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class MinimumWindowSubstring {
 
-    public String minWindow(String s, String t) {
+    public String minWindowN(String s, String t) {
         if (t.isEmpty())
             return "";
 
@@ -47,5 +47,50 @@ public class MinimumWindowSubstring {
         }
 
         return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+    }
+
+    public String minWindow(String s, String t) {
+        if (t.isEmpty())
+            return "";
+
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        }
+
+        int count = t.length();
+        String result = "";
+        int l = 0;
+        int resLen = Integer.MAX_VALUE;
+        for(int r =0; r<s.length();r++){
+            char c = s.charAt(r);
+
+            if (countT.containsKey(c)){
+                countT.put(c,countT.get(c)-1);
+                if(countT.get(c)>=0){
+                    count--;
+                }
+            }
+
+            while (count==0){
+                if(r-l+1 <resLen){
+                    result = s.substring(l,r+1);
+                    resLen = r-l+1;
+                }
+
+                char leftC = s.charAt(l);
+
+                if (countT.containsKey(leftC)){
+                    countT.put(leftC,countT.get(leftC)+1);
+                    if (countT.get(leftC) > 0) {
+                        count++;
+                    }
+                }
+                l++;
+            }
+        }
+
+        return result;
     }
 }
