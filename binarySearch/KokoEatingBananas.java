0bd99627280499306b54d550b129faeddd9a790d
Kaushik <bankarkaushik@gmail.com>
2025-07-26 01:57:27 +0530

    Koko Eating Bananas

---
+++ binarySearch/KokoEatingBananas.java
@@ -0,0 +1,33 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class KokoEatingBananas {
+
+    /**
+     * piles=[3,6,7,11]
+     * h=8
+     */
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = Arrays.stream(piles).max().getAsInt();
+        int res = r;
+
+        while (l <= r) {
+            int k = (l + r) / 2;
+
+            long totalTime = 0;
+            for (int p : piles) {
+                totalTime += Math.ceil((double) p / k);
+            }
+            if (totalTime <= h) {
+                res = k;
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+        return res;
+    }
+
+}
